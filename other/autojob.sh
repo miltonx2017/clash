8b16e07e0b6b81e9bfe4903f1b10b0ddb18fb6b0
root <milton.xue@hotmail.com>
2022-05-04 13:51:44 +0800

    upload

---
+++ other/autojob.sh
@@ -0,0 +1,5 @@
+curl https://cdn.jsdelivr.net/gh/miltonx2017/clash@main/other/urls_changfengoss.txt > /tmp/urls_changfengoss.txt
+curl https://cdn.jsdelivr.net/gh/miltonx2017/clash@main/other/get_changfengoss.sh > /tmp/get_changfengoss.sh
+chmod +x /tmp/get_changfengoss.sh
+cd /tmp
+./get_changfengoss.sh
